a22c5abb376fd8f114ba1025f1a4fabd318b92e3
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-15 15:56:38 +0100

    add cypress e2e and components

---
+++ cypress/e2e/landingPage/initialLoad.cy.ts
@@ -0,0 +1,7 @@
+describe("Initial page load", () => {
+	it("Landing page contains header and navigation bar", () => {
+		cy.visit("/")
+		cy.get("header").should("exist")
+		cy.get("nav").should("exist")
+	})
+})
