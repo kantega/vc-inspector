35fe56a498086430f075ab49fb9e362ae44a1a1d
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-15 12:24:33 +0100

    add typedoc documentation from page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "@/app/utils/styling";
 
 const inter = Inter({ 
   subsets: ["latin"], 
