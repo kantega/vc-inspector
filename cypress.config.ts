99109ccdc5c2ac1728ffa8432e437f92d9eed162
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-16 09:00:28 +0100

    running prettier

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
 
   screenshotOnRunFailure: false,
   video: false,
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {},
   },
 });
