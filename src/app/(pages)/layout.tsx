0f1d6eb76d0c9a1e66a67d2e3cbca92a13386e53
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-16 10:15:56 +0100

    move header and nav into body of layout

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -19,7 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <>
+          <header>
+            <nav></nav>
+          </header>
+          {children}
+        </>
+      </body>
     </html>
   );
 }
