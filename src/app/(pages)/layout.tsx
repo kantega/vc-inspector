f921b11b0aadece21b9016ba8127c8d709f41424
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-16 10:10:00 +0100

    remove cn from layout and its import

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/app/globals.css';
-import { cn } from '@/app/utils/styling';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -20,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('', inter.className)}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
