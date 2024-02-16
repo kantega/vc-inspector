91e0a249334fd622c9e27752488dcb6053ccc55f
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-16 10:06:26 +0100

    change variable to className

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('', inter.variable)}>{children}</body>
+      <body className={cn('', inter.className)}>{children}</body>
     </html>
   );
 }
