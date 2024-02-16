e802f442a7b5eca7485c8d417bcde950254ce1b7
Thomas Svendal <thomassvendal@gmail.com>
2024-02-16 11:01:06 +0100

    Added inpector as its own sub package and a small demo function

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image';
+import inspector from "@/inspector/inspector";
+import Image from "next/image";
 
 export default function Home() {
+  // Proof of concept for the inspector
+  inspector();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
