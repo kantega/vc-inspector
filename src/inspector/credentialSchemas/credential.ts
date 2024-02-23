0df44ab33bcc7bdd7275718e02423dc935d6a96f
Thomas Svendal <thomassvendal@gmail.com>
2024-02-23 12:51:17 +0100

    Removed unused

--- src/inspector/credentialSchemas/credential.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const CredentialSchemaSchema = z.object({
-  type: z.string(),
-  id: z.string().url(),
-});
-
-export type CredentialSchema = z.infer<typeof CredentialSchemaSchema>;
--- src/inspector/credentialSchemas/jsonType.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-const baseJSONTypeSchema = z.union([z.string(), z.number(), z.boolean(), z.null()]);
-
-export type JSONType = z.infer<typeof baseJSONTypeSchema> | JSONType[];
-
-export const JSONTypeSchema: z.ZodType<JSONType> = baseJSONTypeSchema.or(z.lazy(() => JSONTypeSchema.array()));
