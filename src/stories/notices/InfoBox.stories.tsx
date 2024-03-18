d074cbb164d65dafece0726f23e65053d6aa2ce5
Carl Johan Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-18 13:18:10 +0100

    Information box for warnings, notices, errors and success messges. (#28)
    * create component for notices
    
    * create simple story
    
    * update styling for neutral and switch names
    
    * add icons and update colors
    
    * add colors to tailwind config
    
    * update icons to lucide
    
    * change to info box name
    
    * remove icon text color to use the existing color
    
    * add inter font to storybook
    
    * correct success check
    
    * use circle check 2 instead of 1
    
    * update icons with new version of lucide
    
    * semibold and aligned icon with title

---
+++ src/stories/notices/InfoBox.stories.tsx
@@ -0,0 +1,56 @@
+import { StoryObj, Meta } from '@storybook/react';
+import InfoBox from '@/components/notices/InfoBox';
+
+const meta = {
+  title: 'Notices/InfoBox',
+  component: InfoBox,
+  tags: ['autodocs'],
+} satisfies Meta<typeof InfoBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'Information Title',
+    children: 'Information box content',
+    messageType: 'neutral',
+  },
+};
+
+export const WithDefaultNeutral: Story = {
+  args: {
+    title: 'InfoBox Title',
+    children: 'InfoBox content',
+    messageType: 'neutral',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    title: 'Warning about something',
+    children: (
+      <ul className="list-inside list-disc">
+        <li>First item</li>
+        <li>Second item</li>
+      </ul>
+    ),
+    messageType: 'warning',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    title: 'Error message',
+    children: 'An error appeared',
+    messageType: 'error',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    title: 'Successful event message',
+    children: 'Information on what went well',
+    messageType: 'success',
+  },
+};
