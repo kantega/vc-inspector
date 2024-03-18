b1a2c9720099b81400daca06ab5ee1b57f7ec73d
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-18 12:37:55 +0100

    add inter font to storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,21 @@
 import type { Preview } from '@storybook/react';
+import { Inter } from 'next/font/google';
 import '../src/app/globals.css';
+import React from 'react';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <div className={inter.className}>
+        <Story />
+      </div>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {
