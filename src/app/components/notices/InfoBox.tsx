f88bf61fcdc3f7174e824d34b4c1ecb6aa730ea0
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-03-18 12:46:35 +0100

    correct success check

--- src/app/components/notices/InfoBox.tsx
+++ src/app/components/notices/InfoBox.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/utils/styling';
 import { VariantProps, cva } from 'class-variance-authority';
-import { Info, AlertCircle, XCircle, Check, LucideIcon } from 'lucide-react';
+import { Info, AlertCircle, XCircle, LucideIcon, CheckCircle } from 'lucide-react';
 
 const messageType = {
   neutral: 'bg-light-blue text-dark-blue',
@@ -27,7 +27,7 @@ const icons: Record<keyof typeof messageType, LucideIcon> = {
   neutral: Info,
   warning: AlertCircle,
   error: XCircle,
-  success: Check,
+  success: CheckCircle,
 };
 
 /**
