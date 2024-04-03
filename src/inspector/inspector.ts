1454eac55495956303e215fea5dbdce9cc3ba30f
Thomas Svendal <thomassvendal@gmail.com>
2024-04-03 13:50:31 +0200

    Version: 13

--- src/inspector/inspector.ts
+++ src/inspector/inspector.ts
@@ -1,47 +1,49 @@
 import { ZodError } from 'zod';
 import { VC, VCSchema } from './credentialSchemas/verifiableCredential';
-import { parseValidity } from './calculatedAttributes/validity';
+import { parseValidity, Validity, ParserResult } from './calculatedAttributes/validity';
 
 export default function inspect(credential: string): InspectionResult {
-  try {
-    const parsedJson = JSON.parse(credential);
-    const parsedSchema = VCSchema.safeParse(parsedJson);
+	try {
+		const parsedJson = JSON.parse(credential);
+		const parsedSchema = VCSchema.safeParse(parsedJson);
 
-    if (!parsedSchema.success) {
-      return {
-        type: 'InvalidCredential',
-        parsedJson,
-        error: parsedSchema.error,
-      };
-    }
+		if (!parsedSchema.success) {
+			return {
+				type: 'InvalidCredential',
+				parsedJson,
+				error: parsedSchema.error,
+			};
+		}
 
-    console.log('halla', parseValidity(parsedJson));
-    return {
-      type: 'ValidCredential',
-      parsedJson: parsedJson as VC,
-    };
-  } catch (e) {
-    return {
-      type: 'ParseError',
-      error: e as Error,
-    };
-  }
+		console.log('halla', parseValidity(parsedJson));
+		return {
+			type: 'ValidCredential',
+			parsedJson: parsedJson as VC,
+			calculatedAttributes: parseValidity(parsedJson),
+		};
+	} catch (e) {
+		return {
+			type: 'ParseError',
+			error: e as Error,
+		};
+	}
 }
 
 export type InspectionResult =
-  | {
-      type: 'ParseError';
-      error: Error;
-    }
-  | {
-      type: 'InvalidCredential';
-      parsedJson: object;
-      error: ZodError;
-    }
-  | {
-      type: 'ValidCredential';
-      parsedJson: VC;
-    };
+	| {
+		type: 'ParseError';
+		error: Error;
+	}
+	| {
+		type: 'InvalidCredential';
+		parsedJson: object;
+		error: ZodError;
+	}
+	| {
+		type: 'ValidCredential';
+		parsedJson: VC;
+		calculatedAttributes: ParserResult<Validity>;
+	};
 //
 // export type Inspector =
 //   | {
