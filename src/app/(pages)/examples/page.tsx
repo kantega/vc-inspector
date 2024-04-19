6469ef55c2da1e696e18ec70c431254f11892da1
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-04-19 22:56:50 +0200

    add documentation to examples and intro page

--- src/app/(pages)/examples/page.tsx
+++ src/app/(pages)/examples/page.tsx
@@ -1,5 +1,10 @@
 import UnderConstruction from '@/components/notices/UnderConstruction';
 
+/**
+ * A page for giving examples on how Verifiable Credentials
+ * and Presentations might look and what they might contain.
+ * Possible to inspect given examples.
+ */
 export default function Examples() {
   return (
     <main className="p-20">
