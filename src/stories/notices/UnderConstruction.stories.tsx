a6b5e042725e003c810b74453fe796452c675122
Thomas Svendal <thomassvendal@gmail.com>
2024-04-22 09:41:32 +0200

    Fixed storybook and attribute type error

--- src/stories/notices/UnderConstruction.stories.tsx
+++ src/stories/notices/UnderConstruction.stories.tsx
@@ -11,18 +11,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
-export const Small: Story = {
-  args: {
-    size: 'small',
-  },
-};
-export const Medium: Story = {
-  args: {
-    size: 'medium',
-  },
-};
-export const Large: Story = {
-  args: {
-    size: 'large',
-  },
-};
