0007e4c22c54b0b4a339b0b4a456452465c5fc1f
Carl Johan Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-04-24 09:58:48 +0200

    50 47 nested and errors (#72)
    * convert nested claims
    
    * nested claims and errors from credential subject
    
    * show zod errors from issuer
    
    * parse issuer with record string unknown
    
    * move zod formatter out of parsed credential file
    
    * add colons after path and remove slice
    
    * move issue boxes
    
    * larger text for error boxes
    
    * switch from grid to flex
    
    * add back test for missing credential subject
    
    * add documentation to functions

--- src/inspector/assertTypes.ts
+++ src/inspector/assertTypes.ts
@@ -6,3 +6,9 @@ export function isStrRecord(obj: unknown): obj is Record<string, unknown> {
     Object.keys(obj).every((k) => typeof k === 'string')
   );
 }
+
+export type Primitive = string | number | boolean;
+
+export function isPrimitive(obj: unknown): obj is Primitive {
+  return typeof obj === 'string' || typeof obj === 'number' || typeof obj === 'boolean';
+}
