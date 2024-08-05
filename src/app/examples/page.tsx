06df96e338085ca6e98ac9be15da9e1d90dd6571
Erik Skaar <erik.skaar@kantega.no>
2024-08-05 12:50:18 +0200

    example page implemented

--- src/app/examples/page.tsx
+++ src/app/examples/page.tsx
@@ -1,4 +1,7 @@
-import UnderConstruction from '@/components/notices/UnderConstruction';
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import examples from './examples.json';
 
 /**
  * A page for giving examples on how Verifiable Credentials
@@ -7,8 +10,26 @@ import UnderConstruction from '@/components/notices/UnderConstruction';
  */
 export default function Examples() {
   return (
-    <main className="p-20">
-      <UnderConstruction />
-    </main>
+    <div className="flex w-full flex-col items-center gap-8">
+      <h2>Click green links to see the example in VC-inspector. The red ones</h2>
+      {examples.map((example) => (
+        <ExampleLink key={example.title} title={example.title} token={example.token} functional={example.functional} />
+      ))}
+    </div>
+  );
+}
+
+function ExampleLink({ title, token, functional }: { title: string; token: string; functional: boolean }) {
+  return (
+    <Link
+      href={`/token/${token}`}
+      className={cn(
+        buttonVariants({ variant: 'default', size: 'default' }),
+        'h-fit w-96 text-wrap text-xl',
+        functional ? 'bg-green-600' : 'bg-red-600',
+      )}
+    >
+      {title}
+    </Link>
   );
 }
---
+++ src/app/token/[id]/page.tsx
@@ -0,0 +1,18 @@
+import FeedbackBox from '@/components/notices/FeedbackBox';
+import InspectionPage from '@/components/vc-inspection/InspectionPage';
+
+export default async function Home({ params }: { params: { id: string } }) {
+  console.log(params.id);
+  return (
+    <>
+      <InspectionPage defaultToken={base64urlDecode(params.id)} />
+      <FeedbackBox />
+    </>
+  );
+}
+
+function base64urlDecode(input: string): string {
+  const uridecoded = decodeURIComponent(input);
+
+  return uridecoded;
+}
