4e8dd157bb85b2dd633cdc9b17b955bc2bf96e95
Erik Skaar <erik.skaar@kantega.no>
2024-08-08 13:22:57 +0200

    frontend/dont_log_params_when_using_copying_share_vc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+'use client';
 import InspectionPage from '@/components/vc-inspection/InspectionPage';
 
 export default function Home() {
+  if (typeof window !== 'undefined' && window.location.hash !== '') {
+    const token = window.location.hash.split('=')[1].trim();
+    return <InspectionPage defaultToken={base64urlDecode(token)} />;
+  }
   return <InspectionPage />;
 }
+
+const base64urlDecode = (input: string) => decodeURIComponent(input);
