2a0785c731c2981fdae23bae1204bbc18a9b1e4a
Erik Skaar <erik.skaar@kantega.no>
2024-08-20 11:30:37 +0200

    clean up textarea component

--- src/components/vc-inspection/TextArea.tsx
+++ src/components/vc-inspection/TextArea.tsx
@@ -8,14 +8,10 @@ type TextAreaProps = JSX.IntrinsicElements['textarea'];
 const START_HEGHT = 300;
 
 export default function TextArea({ className, ...props }: TextAreaProps) {
-  const ref = useRef<HTMLTextAreaElement | null>(null);
-  useEffect(() => ref.current?.focus(), []);
-
   return (
     <textarea
       {...props}
       aria-label="Verifiable Credential Text Area"
-      ref={ref}
       spellCheck={false}
       placeholder="Paste your verifiable credential here"
       className={cn('h-[50vh] rounded-md bg-light-gray p-4 placeholder-readable-gray', className)}
